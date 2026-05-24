18257ea39dee13a60fd97341ff9fb0560c4b62c0
Rey <gansrey97@gmail.com>
2026-05-24 17:42:40 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// 🔒 FILE RAHASIA - TIDAK AKAN DIUNGGAH KE GITHUB
+const GITHUB_CONFIG = {
+    USERNAME: "inirey",       // Username kamu
+    TOKEN: "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"  // Token asli kamu di sini
+};
