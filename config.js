e155476c39af659eb7efc4257e800fd3c4d356f1
Rey <gansrey97@gmail.com>
2026-05-24 18:01:03 +0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// 🔒 FILE RAHASIA - TIDAK AKAN DIUNGGAH KE GITHUB
-const GITHUB_CONFIG = {
-    USERNAME: "inirey",       // Username kamu
-    TOKEN: "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"  // Token asli kamu di sini
-};
